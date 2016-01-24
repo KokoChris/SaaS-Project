0d03b9ba796d9c9795f35196c6414f9188f4a37a
Chris Kokolios <kokolios1986@yahoo.gr>
2016-01-24 19:00:23 +0200

    Add bootstrap styling

--- config/initializers/milia.rb
+++ config/initializers/milia.rb
@@ -5,17 +5,14 @@ Milia.setup do |config|
 
 # true if you wish to use a coupon-based option in your sign-up form
 # false to otherwise not expect a coupon attribute in the parameters
-  config.use_coupon = true
-
+  config.use_coupon = false 
 # true if you use recaptcha on your sign-up form; 
   # be sure to include the gem 'recaptcha' in your Gemfile
 # false if you do not
-  config.use_recaptcha = true
-
+  config.use_recaptcha = false
 # true if action after signing out is to return to the home (root) page
 # false to return to the sign-in form (devise default)
   config.signout_to_root = true
-
 # true if you are using airbrake to be notified of exceptions
   # be sure to include the gem 'airbrake' in your Gemfile
 # false if you do not
